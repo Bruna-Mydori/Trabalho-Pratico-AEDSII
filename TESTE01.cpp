93fd370ade1915b6edf072dd7fdb01c17a9d5bbf
romulojuniior <romulo20junior@hotmail.com>
2021-04-12 11:56:47 -0300

    Update TESTE01.cpp

--- TESTE01.cpp
+++ TESTE01.cpp
@@ -1,151 +1,285 @@
-#include <iostream>
-#include <vector>
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-using namespace std;
 
-struct projeto{
-	string nome;
-	string datai;
-	string dataf;
-	float preco;
-	float pago;
-};
+////////////////////////////////////////////////////////////////////
+//
+// PROGRAMA EXEMPLO DESENVOLVIDO NAS AULAS DE 16/06 e 20/06/2008
+//
+//   Este programa gerencia um cadastro de funcionarios
+//   implementado em forma de ÁRVORE BINÁRIA DE BUSCA,
+//   com as seguintes funções:
+//
+//     1 - Incluir um novo funcionario
+//     2 - Listar todos os funcionarios
+//     3 - Reajustar o salário de um funcionário
+//     4 - Excluir um funcionario
+//
+////////////////////////////////////////////////////////////////////
 
-struct cliente{
-	string nome;
-	int cpf;
-	string endereco;
-	int telefone;
-	projeto lista;
-	struct cliente *esq;
-    struct cliente *dir;
-};
+typedef struct
+{
+  int matricula;
+  char nome[51];
+  float salario;
+} Funcionario;
 
-typedef struct cliente no;
-typedef no *arvore;
-void inserir (arvore &r, int c, string nome, int tel, string end){
-    if(r==NULL){
-		r = new no;
-		r->cpf = c;
-		r->nome = nome;
-		r->telefone = tel;
-		r->endereco = end;
-		r->esq = NULL;
-		r->dir = NULL;
-    }
-   else {
-		if(nome < r->nome){
-			inserir(r->esq, c, nome, tel, end);
-		}
-		else{
-			inserir(r->dir,c, nome, tel, end);
-		}
-    }
+typedef struct arvore
+{
+  Funcionario raiz;
+  struct arvore *pEsq;
+  struct arvore *pDir;
+} Arvore;
+
+///////////////////////////////////////////////////
+//  Incluir novo funcionario
+///////////////////////////////////////////////////
+
+int incluir(Arvore **ppArvore, Funcionario *pNovoFunc)
+{
+  Arvore *pNovaArvore;
+  
+  // 1. Tratar o caso de arvore vazia
+  
+  if (!(*ppArvore))
+  {
+    pNovaArvore = malloc(sizeof(Arvore)); // alocar um novo no
+    if (!pNovaArvore) return(1);          // erro de alocacao de memoria
+    pNovaArvore->raiz = *pNovoFunc;
+    pNovaArvore->pEsq = NULL;
+    pNovaArvore->pDir = NULL;
+    *ppArvore = pNovaArvore;
+    return(0);
+  }
+
+  // 2. Se a nova matricula for menor que a matricula armazenada na raiz,
+  //      incluir a nova matricula na subarvore esquerda
+
+  if ( pNovoFunc->matricula < (*ppArvore)->raiz.matricula )
+    return incluir(&((*ppArvore)->pEsq),pNovoFunc);
+
+  // 3. Se a nova matricula for maior que a matricula armazenada na raiz,
+  //      incluir a nova matricula na subarvore direita
+
+  if ( pNovoFunc->matricula > (*ppArvore)->raiz.matricula )
+    return incluir(&((*ppArvore)->pDir),pNovoFunc);
+
+  // 4. Se a matricula ja existir, retornar erro 2
+  
+  return(2);
 }
-void erd(arvore r){
-    if(r!=NULL){
-        erd(r->esq);
-        cout << "- " << r->nome << endl;
-        erd(r->dir);
-    }
+
+///////////////////////////////////////////////////
+//  Listar funcionarios
+///////////////////////////////////////////////////
+
+void listarFuncionarios(Arvore *pArvore)
+{
+    if (!pArvore) return;
+    listarFuncionarios(pArvore->pEsq);
+    printf("%9d  %-10s  %7.2f  %p  %p  %p\n",
+           pArvore->raiz.matricula,
+           pArvore->raiz.nome,
+           pArvore->raiz.salario,
+           pArvore,
+           pArvore->pEsq,
+           pArvore->pDir);
+    listarFuncionarios(pArvore->pDir);
 }
 
-void red(cliente a){
-    if(cliente a!=NULL){
-        erd(a->esq);
-        cout << "- " << a.lista.nome << endl;
-        erd(a->dir);
-    }
+///////////////////////////////////////////////////
+//  Reajustar salario de um funcionario
+///////////////////////////////////////////////////
+
+int reajustar(Arvore *pArvore, int matricula, float reajuste)
+{
+
+  // 1. Se a arvore estiver vazia, retornar erro 1
+
+  if (!pArvore) return(1);   // elemento nao encontrado
+
+  // 2. Se a matricula do funcionario for menor que a matricula armazenada na raiz,
+  //      procurar matricula na subarvore esquerda
+
+  if ( matricula < pArvore->raiz.matricula )
+    return reajustar(pArvore->pEsq,matricula,reajuste);
+
+  // 3. Se a matricula do funcionario for maior que a matricula armazenada na raiz,
+  //      procurar matricula na subarvore direita
+
+  if ( matricula > pArvore->raiz.matricula )
+    return reajustar(pArvore->pDir,matricula,reajuste);
+
+  // 4. Funcionario encontrado: reajustar salario
+
+  pArvore->raiz.salario *= 1 + reajuste/100;
+  
+  return(0);
+  
 }
 
-/*no *busca(arvore r, int k){
-	if(r==NULL || r->conteudo.nome == k)
-		return r;
-	if(r->conteudo.nome > k)
-		return busca(r->esq, k);
-	else
-		return busca(r->dir, k);
-}*/
-int main(){
-	arvore r; r = NULL;
-	cliente a;
-	string nome, end;
-	int x, c, tel;
-	cout << "Menu" << endl;
-	cout << "1 - Incluir um cliente na lista\n2 - Associar um projeto a um cliente \n3 - Imprimir a lista de clientes\n4 - Imprimir a lista de projetos\n5 - Imprimir a lista de clientes com seus respectivos projetos\n6 - Informar total de valor a receber\n7 - Informar total de valor já recebido\n8 - Pesquisar um projeto\n9 - Pesquisar um cliente\n10 - Remover um projeto de um cliente\n11 - Remover um cliente\n12 - Listar clientes que ainda devem e quais projetos ainda não foram pagos\n0 - Encerrar o programa\n";
-	cin >> x;
-	while (x!=0){
-		if (x==1){
-			cout << "Digite o nome do cliente(use _ em vez de espaço): ";
-			cin >> nome;
-			cout << "Digite o CPF(apenas números): ";
-			cin >> c;
-			cout << "Digite o telefone(apenas números): ";
-			cin >> tel;
-			cout << "Digite o endereço(use _ em vez de espaço): ";
-			cin >> end;
-			inserir(r, c, nome, tel, end);
-		}
-		if (x==2){
-			cout << "Digite o nome do projeto(use _ em vez de espaço): ";
-			cin >> a.lista.nome;
-			cout << "Digite a data inicial do projeto: ";
-			cin >> a.lista.datai;
-			cout << "Digite a data fianl do projeto: ";
-			cin >> a.lista.dataf;
-			cout << "Digite o preço total do projeto: ";
-			cin >> a.lista.preco;
-			cout << "Digite o valor recebido do projeto: ";
-			cin >> a.lista.pago;
-			cout << "Digite o nome do cliente(use _ em vez de espaço): ";
-			cin >> a.nome;
-		}
-		if (x==3){
-			erd(r);
-			system("pause");
-		}
-		if (x==4){
-			red(cliente a);
-			system("pause");
-		}
-		if (x==5){
-			cout << "Digite um nome: ";
-			cin >> nome;
-			if(nome==a.nome){
-				cout << "Nome projeto: " << a.lista.nome << endl;
-				cout << "Data inicial: " << a.lista.datai << endl;
-				cout << "Data final: " << a.lista.dataf << endl;
-				cout << "Valor: " << a.lista.preco << endl;
-				cout << "Pago: " << a.lista.pago << endl;
-				system("pause");
-			}
-		}
-		if (x==6){
-			cout << "f" << endl;
-		}
-		if (x==7){
-			cout << "g" << endl;
-		}
-		if (x==8){
-			cout << "h" << endl;
-		}
-		if (x==9){
-			cout << "i" << endl;
-		}
-		if (x==10){
-			cout << "j" << endl;
-		}
-		if (x==11){
-			cout << "k" << endl;
-		}
-		if (x==12){
-			cout << "l" << endl;
-		}
-		system("cls");
-		cout << "1 - Incluir um cliente na lista\n2 - Associar um projeto a um cliente \n3 - Imprimir a lista de clientes\n4 - Imprimir a lista de projetos\n5 - Imprimir a lista de clientes com seus respectivos projetos\n6 - Informar total de valor a receber\n7 - Informar total de valor já recebido\n8 - Pesquisar um projeto\n9 - Pesquisar um cliente\n10 - Remover um projeto de um cliente\n11 - Remover um cliente\n12 - Listar clientes que ainda devem e quais projetos ainda não foram pagos\n0 - Encerrar o programa\n";
-		cin >> x;
-	}
-	cout << "Programa encerrado" << endl;
+
+///////////////////////////////////////////////////
+//  Excluir funcionario
+///////////////////////////////////////////////////
+
+
+int excluir(Arvore **ppArvore, int matriculaExcluir)
+{
+  Arvore *pVerticeExcluir, *pSucessor, **ppSucessor;
+
+  // 1. Se a arvore estiver vazia, retornar erro 1
+
+  if (!(*ppArvore)) return(1);   // elemento nao encontrado
+
+  // 2. Se a matricula a ser excluida for menor que a matricula armazenada na raiz,
+  //      excluir matricula na subarvore esquerda
+
+  if ( matriculaExcluir < (*ppArvore)->raiz.matricula )
+    return excluir(&((*ppArvore)->pEsq),matriculaExcluir);
+
+  // 3. Se a matricula a ser excluida for maior que a matricula armazenada na raiz,
+  //      excluir matricula na subarvore direita
+
+  if ( matriculaExcluir > (*ppArvore)->raiz.matricula )
+    return excluir(&((*ppArvore)->pDir),matriculaExcluir);
+
+  // 4. Aqui o vertice a ser excluido foi encontrado (eh a raiz da arvore)
+
+  pVerticeExcluir = *ppArvore;
+  
+  // 5. Se o vertice a ser excluido nao tiver filho esquerdo,
+  //      o vertice-pai passa a apontar o filho direito do vertice a ser excluido
+
+  if (!((*ppArvore)->pEsq))
+  {
+     *ppArvore = (*ppArvore)->pDir;
+  }
+
+  // 6. Se o vertice a ser excluido tiver filho esquerdo mas nao tiver filho direito,
+  //      o vertice-pai passa a apontar o filho esquerdo do vertice a ser excluido
+
+  else if ((!(*ppArvore)->pDir))
+  {
+     *ppArvore = (*ppArvore)->pEsq;
+  }
+
+  // 7. Se o vertice a ser excluido tiver 2 filhos
+  //
+  //    7.1. localizar o sucessor do vertice a ser excluido
+  //         (sucessor = descendente mais a esquerda do filho direito)
+  //
+  //    7.2. pai do sucessor passa a apontar filho direito do sucessor
+  //
+  //    7.3. sucessor passa a ocupar o lugar do vertice a ser excluido (raiz da arvore)
+
+  else
+  {
+     // 7.1. localizar o sucessor do vertice a ser excluido
+     //      (sucessor = descendente mais a esquerda do filho direito)
+
+     ppSucessor = &(pVerticeExcluir->pDir);
+     while ((*ppSucessor)->pEsq) ppSucessor = &((*ppSucessor)->pEsq);
+     pSucessor = *ppSucessor;
+     
+     // 7.2. pai do sucessor passa a apontar filho direito do sucessor
+     
+     *ppSucessor = pSucessor->pDir;
+
+     // 7.3. sucessor passa a ocupar o lugar do vertice a ser excluido (raiz da arvore)
+     
+     *ppArvore = pSucessor;
+     pSucessor->pEsq = pVerticeExcluir->pEsq;
+     pSucessor->pDir = pVerticeExcluir->pDir;
+
+  }
+      
+  // 8. Desalocar memoria ocupada pela antiga posicao do sucessor
+  
+  free(pVerticeExcluir);
+
+  // 9. Retornar codigo de exclusao bem sucedida
+
+  return(0);
+  
 }
+
+
+
+
+///////////////////////////////////////////////////
+//  PROGRAMA PRINCIPAL
+///////////////////////////////////////////////////
+
+int main()
+{
+  char opcao[200];
+  int op, codRetorno, matricula;
+  float reajuste;
+  Funcionario novoFuncionario;
+  Arvore *pCadastro = NULL;
+
+  do
+  {
+    system("cls");
+    printf("1 - Incluir um novo funcionario\n");
+    printf("2 - Listar todos os funcionarios\n");
+    printf("3 - Reajustar o salário de um funcionario\n");
+    printf("4 - Excluir um funcionario\n");
+    printf("5 - Sair do programa\n");
+    printf("\nOpcao: ");
+    scanf("%s",opcao);
+    op = atoi(opcao);
+
+    // 1 - Incluir um novo funcionario
+    
+    if (op==1)
+    {
+       printf("\n\n\n");
+       printf("Nome: ");      scanf("%s",novoFuncionario.nome);
+       printf("Matricula: "); scanf("%d",&(novoFuncionario.matricula));
+       printf("Salario: ");   scanf("%f",&(novoFuncionario.salario));
+       codRetorno = incluir(&pCadastro,&novoFuncionario);
+       if (codRetorno==0) printf("\nInclusao bem sucedida\n\n");
+       if (codRetorno==1) printf("\nERRO: memoria insuficiente\n\n");
+       if (codRetorno==2) printf("\nERRO: matricula duplicada\n\n");
+    }
+
+    // 2 - Listar todos os funcionarios
+
+    if (op==2)
+    {
+       printf("\n\n                                Endereco  Endereco  Endereco");
+       printf(  "\nMatricula  Nome        Salario  Proprio   Esquerdo  Direito\n\n");
+       listarFuncionarios(pCadastro);
+       printf("\n\n");
+    }
+       
+    // 3 - Reajustar o salario de um funcionario
+
+    if (op==3)
+    {
+       printf("\n\n\n");
+       printf("Matricula do Funcionario: "); scanf("%d",&matricula);
+       printf("Percentual de Reajuste:   "); scanf("%f",&reajuste);
+       codRetorno = reajustar(pCadastro,matricula,reajuste);
+       if (codRetorno==0) printf("\nAtualizacao bem sucedida\n\n");
+       if (codRetorno==1) printf("\nERRO: matricula nao encontrada\n\n");
+    }
+
+    // 4 - Excluir um funcionario
+
+    if (op==4)
+    {
+       printf("\n\n\n");
+       printf("Matricula: "); scanf("%d",&matricula);
+       codRetorno = excluir(&pCadastro,matricula);
+       if (codRetorno==0) printf("\nExclusao bem sucedida\n\n");
+       if (codRetorno==1) printf("\nERRO: matricula nao encontrada\n\n");
+    }
+
+
+    if (op != 5) system("pause");
+
+  } while (op != 5);
+}
+  
