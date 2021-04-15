f9f52b92d348aca25c93a50a18e8e482cbbcbed3
romulojuniior <romulo20junior@hotmail.com>
2021-04-15 11:09:16 -0300

    Update teste.cpp

--- teste.cpp
+++ teste.cpp
@@ -230,9 +230,12 @@ int main(){
 		}
 		if (x==7){
 			cout << "g" << endl;
+			/*cout << "Total recebido: " << endl; */
 		}
 		if (x==8){
 			cout << "h" << endl;
+			/*cout << "Cliente: " << endl; */
+			/*cout << "Projeto: " << endl; */
 		}
 		if (x==9){
 			cout << "i" << endl;
@@ -249,6 +252,8 @@ int main(){
 		}
 		if (x==12){
 			cout << "l" << endl;
+			/*cout << "Clientes que ainda devem: " << endl;
+			cout << "Projetos que ainda nao foram pagos: " << endl;*/
 		}
 		system("cls");
 		cout << "1 - Incluir um cliente na lista\n2 - Associar um projeto a um cliente \n3 - Imprimir a lista de clientes\n4 - Imprimir a lista de projetos\n5 - Imprimir a lista de clientes com seus respectivos projetos\n6 - Informar total de valor a receber\n7 - Informar total de valor ja recebido\n8 - Pesquisar um projeto\n9 - Pesquisar um cliente\n10 - Remover um projeto de um cliente\n11 - Remover um cliente\n12 - Listar clientes que ainda devem e quais projetos ainda nao foram pagos\n0 - Encerrar o programa\n";
